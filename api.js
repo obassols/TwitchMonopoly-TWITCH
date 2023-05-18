16d660ccc245c9feb4ff311bb0014ba6a1fb918e
obassols <obassols8@boscdelacoma.cat>
2023-05-18 19:05:01 +0200

    Api for nodejs

---
+++ api.js
@@ -0,0 +1,59 @@
+const request = require('request');
+const serverUrl = 'localhost';
+const port = 4000;
+
+function getSquares(game) {
+  const url = 'http://' + serverUrl + ':' + port + '/square';
+  request(url, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    game.squares = body;
+    console.log('Squares loaded');
+    const index = require('./index');
+    index.updateGame(game);
+  });
+}
+
+async function getGameSquares(id) {
+  const url = 'http://' + serverUrl + ':' + port + '/square/game/' + id;
+  request(url, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    game.squares = body;
+    console.log('Squares loaded');
+    const index = require('./index');
+    index.updateGame(game);
+  });
+}
+
+function getRents(game) {
+  console.log('Getting rents');
+  console.log(game.actualSquare);
+  
+  const url = 'http://' + serverUrl + ':' + port + '/square/' + game.actualSquare.id + '/rent';
+  request(url, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    game.actualSquare.rents = body;
+    console.log(game.actualSquare.rents);
+    console.log('Rents loaded');
+
+    const index = require('./index');
+    index.updateGame(game);
+  });
+}
+
+function getCard(game, type) {
+  const url = 'http://' + serverUrl + ':' + port + '/card/random/' + type;
+  request(url, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body);
+    game.actualCard = body;
+    const monopoly = require('./monopoly');
+    monopoly.calcCardActions(game);
+  });
+}
+
+module.exports = {
+  getSquares,
+  getGameSquares,
+  getRents,
+  getCard
+};
